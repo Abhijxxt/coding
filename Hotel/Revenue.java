474003236a8813e23035c77eaf8a7780c22e9bee
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2023-06-05 00:47:48 +0530

    fixes

--- Hotel/Revenue.java
+++ Hotel/Revenue.java
@@ -1,17 +1,18 @@
 package Hotel;
 
 import Hotel.Persons;
-import java.util.*;
+//import java.util.*;
 
 public class Revenue {
     String Name;
+    
     Revenue()
     {
         Name="";
     }
     public void Guest()
     {
-        Persons name = new Persons();
+        Hotel.Persons name = new Hotel.Persons();
         System.out.println(name.name());  
         
     }

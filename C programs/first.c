aa03c96981121de9c5628b440363bd27b5bb3bbf
Abhijxxt <abhijeetchatterjee2004@gmail.com>
2023-06-05 13:33:36 +0530

    more c updates

--- C programs/first.c
+++ C programs/first.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
-#include<string.h>
+#include<stdlib.h> //random function is implemented here
+#include<math.h> // for math pow ceil floor
+#include<string.h> // for strcpy strcmp string work
+#include<stdbool.h> //for boolean
+
+struct Person
+{
+    char firstName[50];
+    char lastName[50];
+    int age;
+    double rating;// 1 to 5
+    char gradePay; //A,B,C
+    char bio[100];
+}; //struct is like a class with only global variables in java, acting as an object.
+
 
 void arrays(); /*initialization to remove error while function calling through main.
  Declaring method above main can also be done(PREFERRED)*/ 
+
+void dictionary(); 
 
 void DataTypesAndInputs()
 {
@@ -64,28 +78,83 @@ void userIdPass()
 {
     char id[10];
     char password[10];
-    printf("\n\nEntering User Credential system\n");
-    printf("Enter your name(Ans:Abhijeet): ");
-    scanf("%s",id);
-    printf("Enter your password(Ans:Root): ");
-    scanf("%s", password);
+    bool flag = false;
+    
+    while (!flag)
+    {
+        printf("\n\nEntering User Credential system\n");
+        printf("Enter your name(Ans:Abhijeet): ");
+        scanf("%s",id);
+        printf("Enter your password(Ans:Root): ");
+        scanf("%s", password);
 
-    //if(id == "Abhijeet" && password == "Root") you can't use this for string
-    if(strcmp(id,"Abhijeet")==0 && strcmp(password, "Root") == 0)
-    {
-        printf("Welcome User. Logged in Successfully.");
-    } 
-    else
-    {
-        printf("Login failed.");
+        //if(id == "Abhijeet" && password == "Root") you can't use this for string
+        if(strcmp(id,"Abhijeet")==0 && strcmp(password, "Root") == 0)
+        {
+            printf("Welcome User. Logged in Successfully.");
+            flag = true;
+            
+        } 
+        else if (strcmp(id,"Abhijeet")==0 && strcmp(password, "Root") != 0)
+        {
+            printf("User id correct but password wrong.");
+            
+        }
+        else
+        {
+            printf("Login failed.");
+            
+        }
     }
+    
+    
+    /*
+    if() {}
+    else if() {}
+    else() {}
+    can also be used in various situations
+    */
+}
+
+void Struct() {
+    struct Person Person1;
+    strcpy(Person1.firstName, "Abhijeet");
+    strcpy(Person1.lastName, "Chatterjee");
+    strcpy(Person1.bio, "I am an amateur programmer.");
+    Person1.age = 19;
+    Person1.rating = 4.2;
+    Person1.gradePay = 'C';
+    printf("Name: %s %s\nAge: %d\nRating: %f\nGrade Pay: %c\nBio: %s",
+    Person1.firstName,Person1.lastName, Person1.age, Person1.rating, Person1.gradePay, Person1.bio);
+}
+
+void GuessGame() {
+    int random = rand();
+    printf("%d", random);
+    bool flag = false;
+    int num;
+    while(!flag)
+    {
+        printf("\nGuess the number\n->");
+        scanf("%d",&num);
+        if(num == random)
+        {
+            printf("Woohhoooo! You guessed it right!");
+            flag = true;
+        }
+        else
+        {
+            printf("Wrong. Guess again.");
+        }
+    }
+
 }
 
 int main()
 {
     //Data type and Inputs
     printf("\nDo you want to enter Data Type and Input section?y/n");
-    printf("\nPRESS ENTER TO SKIP AS WELL\n->");
+    printf("\n->");
     char x;
     scanf("%c", &x);
     if (x == 'y' || x=='Y')
@@ -93,9 +162,10 @@ int main()
         DataTypesAndInputs();
     }
     scanf("%c", &x); //Just to bypass auto input
+
     //Arrays
     printf("\nDo you want to enter Arrays section?y/n");
-    printf("\nPRESS ENTER TO SKIP AS WELL\n->");
+    printf("\n->");
     char x2;
     scanf("%c", &x2);
     if (x2 == 'y' || x2=='Y')
@@ -103,9 +173,10 @@ int main()
         arrays();
     }
     scanf("%c", &x); //Just to bypass auto input
-    //Credentials
+
+    //Credentials( if else if else )
     printf("\nDo you want to enter Credential login section?y/n");
-    printf("\nPRESS ENTER TO SKIP AS WELL\n->");
+    printf("\n->");
     char x3;
     scanf("%c", &x3);
     if (x3 == 'y' || x3=='Y')
@@ -114,11 +185,44 @@ int main()
     }
     scanf("%c", &x); //Just to bypass auto input
     
+    //Name dictionary (Switch case)
+    printf("\nDo you want to enter Dictionary section?y/n");
+    printf("\n->");
+    char x4;
+    scanf("%c", &x4);
+    if (x4 == 'y' || x4=='Y')
+    {
+        dictionary();
+    }
+    scanf("%c", &x); //Just to bypass auto input
+    
+    //struct usage and learning
+    printf("\nDo you want to enter Struct learning section?y/n");
+    printf("\n->");
+    char x5;
+    scanf("%c", &x5);
+    if (x5 == 'y' || x5=='Y')
+    {
+        Struct();
+    }
+    scanf("%c", &x); //Just to bypass auto input
+    
+    //Guessing Game(Usage of random)
+    printf("\nDo you want to enter Guessing Game?y/n");
+    printf("\n->");
+    char x6;
+    scanf("%c", &x6);
+    if (x6 == 'y' || x6=='Y')
+    {
+        GuessGame();
+    }
+    scanf("%c", &x); //Just to bypass auto input
 
     return 0;
 }
 
 void arrays()
+
 {
     printf("\n\nENTERING ARRAYS SECTION.\n");
     int numArr[] = {11,22,33,44,55};
@@ -127,3 +231,40 @@ void arrays()
     printf("Value at 0 position is %d", numArr[0]);
     //printf("%s", stringArr[0]);
 }
+
+void dictionary()
+{
+    //You cant switch names
+    printf("\nENTERING DICTIONARY.");
+    printf("\nEnter a number from 1-4: \n");
+    int number;
+    scanf("%d",&number);
+    switch (number)
+    {
+    case 1:
+        printf("1 is denoted to the Code creator: Abhijeet Chatterjee");
+        break;
+    case 2:
+        printf("2 is denoted to the girlfriend of Code creator: Soniya Mehra");
+        break;
+    case 3:
+        printf("3 is denoted to the friend of Code Creator: Sanskar Singh");
+        break;
+    case 4:
+        printf("4 is denoted to the gaming buddy of Code Creator: Pranav Dubey");
+        break;
+    default:
+        printf("No one is denoted with this code!");
+        break;
+    }
+}
+
+/*
+while(){
+
+}
+
+do{
+
+}while();
+*/
